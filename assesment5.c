b7d9ee8e3c4a67f4278a2ad80adc3f29d3d5e8e2
Rohi0150 <girirohi03@gmail.com>
2025-12-30 20:11:05 +0530

    for loops

--- assesment5.c
+++ assesment5.c
@@ -161,9 +161,152 @@ int main(){
   }
   if (flag ==1) printf("Not a prime number");
   else printf("Prime");*/
-  /*20.Write a program print total number of single digit Prime numbers*/
-  for (int x = 2 ; x <10 ; x++){
-    for (int i = 2 ; i<x ;)
+  /*20.Write a program print total number of single digit Prime numbers
+  int sum =0,flag ,count ;
+  for (int x = 2 ; x< 10 ; x++){
+    for (int i = 2 ; i<x ;i++){
+       flag =0;
+      if (x!=i && x%i==0){
+        flag =1;
+        break;
+      }
+    }
+    if (flag ==1 ) ;
+    else count++;
   }
-  
+  printf("%d",count);*/
+
+  /*21.Write a program get number from user print the total number digits which are odd in the number.
+  int x ,y,count =0 ;
+  scanf("%d",&x);
+  for (x ; x>0 ;x/=10)
+  {
+    y = x%10;
+    if (y %2==1)count++;
+
+  }
+  printf("%d",count);
+  */
+ /*22.Write a program get number from user print the total number of two-digit odd numbers in the number.
+ int x ,y,count=0;
+ scanf("%d",&x);
+ for (x; x>0;x/=10){
+  y = x%10;
+  if (y%2==1) count++;
+ }
+ printf("%d",count-1);*/
+ /*23.Write a program get number from user print the total number of single-digit perfect square numbers in the number.
+  int x,count =0 ;
+  scanf ("%d",&x);
+  for (x ; x>0;x/=10){
+    int y = x%10;
+    if (y == 1 ||y == 4 ||y==9){
+      count ++;
+    }
+  }
+  printf("%d",count);*/
+  /*24.Write a program get number from user print the total number of two-digit perfect square numbers in the number
+  int y,x ;
+  scanf("%d",&x);
+  int z = x,count =1;
+  for (x; x>10 ;x/=10){
+    count = count *10;
+  }
+  int m =count /10;
+  int l, c=0 ;
+  for (int i =1 ; i<10;i++){
+    y=i*i;
+    for(l = 1; l<=m ;l=l*10){
+        if (y==(z/(m/l))%100) c++;
+    }
+  }
+  printf("%d",c);*/
+
+  /*25.Write a program get number from user print the total number of single-digit prime numbers in the number.
+  int x,flag;
+  scanf("%d",&x);
+  int y,count=0;
+  for (x; x>0 ; x/=10){
+    y = x%10;
+    if (y==1) continue;
+    for (int i =2 ; i < y ;i++){
+      flag =0;
+      if(y%i==0 ){
+        flag =1;
+        break;
+      } 
+    }
+    if (flag == 1 ){;
+    }
+    else {count++;
+    printf("%d",count;)}
+  }*/
+ /*26.Write a program to print biggest 4-digit number which is divisible by 7 and 9.
+ for (int i = 9999; i>=1000 ;i--){
+  if (i%7==0 && i%9==0){
+    printf("%d",i);
+    break;
+  }
+ }*/
+
+ /*27.Write a program to print the total count of numbers which are less than 100000 and whose sum of digits is 14.
+ int x ,sum ,count=0 ;
+ for (int i = 1 ; i<99999;i++){
+  x=i;
+  sum =0 ;
+  for (;x>0;x/=10){
+    sum = sum+(x%10);
+  }
+  if (sum == 14){
+    count++;
+  }
+
+ }
+ printf("%d",count);
+  */
+    /*28.Write a program to get two numbers from user and print the LCM of those numbers.
+    int x,y,z,lcm;
+    scanf("%d",&x);
+    scanf("%d",&y);
+    z= (x>y)?x:y;
+
+    for (int i = z ; i<=x*y ;i++){
+      if(i%x==0 && i%y==0 ){
+        lcm = i;
+        break;
+      }
+    }
+    printf("%d",lcm);
+    */
+   /*29.Write a program to get three numbers from user and print the LCM of those numbers
+   int x,y,z,max,lcm =0;
+   scanf("%d",&x);
+   scanf("%d",&y);
+   scanf("%d",&z);
+   if(x>y&& x>z) max = x;
+   else if (y>x && y>z) max = y;
+   else max = z;
+
+   for (int i =max ; i< x*y*z ; i++){
+    if (i%x==0 && i%y==0 && i%z==0){
+      lcm =i;
+      break;
+    }
+   }
+   printf("%d",lcm);*/
+   /*30.Write a program to get two numbers from user and print the HCF of those numbers*
+   int x,y,lcm=0;
+   scanf("%d",&x);
+   scanf("%d",&y);
+   int max = x>y ? x:y;
+   for (int i =max ; i<= x*y ;i++ ){
+    if(i%x==0 && i%y==0){
+      lcm =i;
+      break;
+    }
+   }
+   int gcd = x*y /lcm;
+   printf("%d",gcd);
+*/
+
 }
