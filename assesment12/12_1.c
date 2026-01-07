95bd5a62901771cbac72f70896f237cbc0e33185
Rohi0150 <girirohi03@gmail.com>
2026-01-07 08:53:39 +0530

    Pointers

--- assesment12/12_1.c
+++ assesment12/12_1.c
@@ -1,22 +1,43 @@
 /*Question:
-Get two numbers of up to 50 digits and perform addition and print the result.
+
+Get two numbers of up to 50 digits and perform addition and print the result.*/
 #include <stdio.h>
+#include <string.h>
 void getnumbers(char * number1,char * number2){
-    char number1[51] = {0};
-    int i =0 ;
-    while(number1[i]!='\n'&&i<50){
-        scanf("%c",&number1[i]);
-        i++;
+
+    fgets (number1,50,stdin);
+    number1[strcspn(number1,"\n")]='\0';
+    fgets (number2,50,stdin);
+    number2[strcspn(number2,"\n")]='\0';
+}
+
+void addnumbers(char * number1,char * number2,char * result){
+    int n1 = strlen(number1)-1;    
+    int n2 = strlen(number2)-1;
+    // printf("%d",n1);    
+    // printf("%d",n2);
+    int max = n1>n2 ? n1:n2;
+    int carry =0;
+    result[max+1]='\0';
+    for (;n1>=0 || n2>=0 ;max--){
+        int a=0 ;
+        int b=0 ;
+        if(number1[n1]>='0' && number1[n1]<='9') a= number1[n1]-'0';
+        if(number2[n2]>='0' && number2[n2]<='9') b= number2[n2]-'0';
+        int sum = a+b+carry;
+        result[max] = (sum%10 )+'0';
+        carry = sum/10;
+        n1--;
+        n2--;
     }
 }
-int* addnumbers(int * number1,int * number2,int * result){
-    for (int i =0 ;i<;i++){
-
-    }
+void print(char * result){
+    
+    printf("%s",result);
 }
 int main(){
-    char number1[50],number2[50],result[50];
+    char number1[50],number2[50],result[51]={0};
     getnumbers(number1,number2);
-    result = addnumbers(number1,number2,result);
-    printf("%d",result);
-}*/
+    addnumbers(number1,number2,result);
+    print(result);
+}
--- assesment12/12_3.c
+++ assesment12/12_3.c
@@ -6,35 +6,34 @@ string: helloetalvislearning
 
 sub-string: etal
 
-Answer: 6
+Answer: 6*/
 #include <stdio.h>
 #include <string.h>
 int sliceString(char * name,char *ss){
-    int i = 0 ;
-    int j = 0 ;
-    int res = 0;
-    int flag = 0;
-    while(name[i]!=0){
-        if(name[i]==ss[j]){
-            while(ss[j]!='\0'){
-                if (name[i]==ss[j]){
-                    flag = 1;
-                    i++;
-                    j++;
-                }
+    int j =0 ;
+    for(int i =0 ;name[i]!='\0';i++){
+        for (j =0 ; ss[j]!='\0';j++){
+            if(name[i+j]!=ss[j]){
+                break;
             }
+            
         }
-        i++;
+        if(ss[j]=='\0'){
+                
+                return i+1;
+            }
     }
 }
+
 int main(){
     char name[30];
     fgets(name,30,stdin);
-    name[strcspn(name,'\n')]='\0';
+    name[strcspn(name,"\n")]='\0';
     
     char ss[30];
     fgets(ss,30,stdin);
-    ss[strcspn(ss,'\n')]='\0';
+    ss[strcspn(ss,"\n")]='\0';
 
-    sliceString(name,ss);
-}*/
+    int y = sliceString(name,ss);
+    printf("%d",y);
+}
